138355fb8dcc1f3592897ce472a68c9f76fc4e85
SanyaSho <gamep8118@gmail.com>
2022-04-27 22:32:31 +0300

    update resourcepack

--- resourcepack/genrphash.sh
+++ resourcepack/genrphash.sh
@@ -1,6 +1,9 @@
 #!/bin/zsh
 export RP=resourcepack.zip
+export RP_MUSIC=resourcepack_music.zip
 export TXT=resourcepack.txt
 
 echo "Hashing..." $RP "to" $TXT
 sha1sum $RP > $TXT
+echo "Hashing..." $RP_MUSIC "to" $TXT
+sha1sum $RP_MUSIC >> $TXT
